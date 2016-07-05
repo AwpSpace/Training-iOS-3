7965d5feddf98c9e74e6ec02b469ed05169c5d6b
HaiNguyen <tuanhaicntt@gmail.com>
2016-07-05 09:23:24 +0700

    Add training 3

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'Training-iOS-3' do
+    pod 'Alamofire', '~> 3.4'
+    pod 'Kingfisher', '~> 2.4'
+    pod 'R.swift'
+end
+
---
+++ Training-iOS-3/View/ExplorePhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExplorePhotoCollectionViewCell.swift
+//  Training-iOS-3
+//
+//  Created by Tuan Hai on 6/20/16.
+//  Copyright © 2016 Awpspace. All rights reserved.
+//
+
+import UIKit
+
+class ExplorePhotoCollectionViewCell: UICollectionViewCell {
+    
+}
